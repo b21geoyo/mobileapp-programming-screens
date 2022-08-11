646d3767b6f03d2e2a7cf975d89f8dd90fb8cd32
b21geoyo <b21geoyo@student.his.se>
2022-08-11 16:17:55 +0200

    Added data to intent bundle using extras

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button send;
+    private EditText text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +19,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         send = findViewById(R.id.button);
+        text = findViewById(R.id.editText);
 
         send.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            intent.putExtra("Text", text.getText().toString());
+
             startActivity(intent);
         });
     }
