372fe2eea3fd964b5a73a678207dd736f69fc5fc
b21geoyo <b21geoyo@student.his.se>
2022-08-11 16:19:13 +0200

    Added textview to second activity to show intent data

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -2,13 +2,25 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private TextView TextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        TextView = findViewById(R.id.textView);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String text = extras.getString("Text");
+
+        TextView.setText(text);
     }
 }
